4be0eb595b44f90e7f5f7305b391040ad8e630ca
abubakkar102002-byte <abubakkar102002@gmail.com>
2026-08-13 17:54:50 +0300

    Add test for StructureAnalyzer functionality

--- test_structure.py
+++ test_structure.py
@@ -1,1 +1,18 @@
+# test_structure.py
+import pandas as pd
+from strategy.structure import StructureAnalyzer
 
+def test():
+    data = {
+        'open': [10, 12, 11, 15, 14, 18],
+        'high': [11, 13, 12, 16, 15, 19],
+        'low': [9, 11, 10, 14, 13, 17],
+        'close': [12, 11, 15, 14, 18, 17]
+    }
+    df = pd.DataFrame(data)
+    analyzer = StructureAnalyzer(swing_window=1)
+    res = analyzer.analyze_structure(df)
+    print("Structure Test Result:", res)
+
+if __name__ == "__main__":
+    test()
